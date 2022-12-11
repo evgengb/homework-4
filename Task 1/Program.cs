14f3792d67916c6af68555f11604a96df8848f54
Evgen <beu08@list.ru>
2022-12-11 18:23:53 +0600

    Task 1

---
+++ Task 1/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и 
+// возводит число A в натуральную степень B. Реализовать через функции.
+
+int Input(string message)
+{
+    System.Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int numberA = Input("Введите значение А: ");
+int numberB = Input("Введите значение B: ");
+ToDegree(numberA, numberB);
+
+void ToDegree(int a, int b)
+{
+    int result = 1;
+    for (int i = 1; i <= b; i++)
+    {
+        result = result * a;
+    }
+    Console.WriteLine(result);
+}
+
+
+
+
